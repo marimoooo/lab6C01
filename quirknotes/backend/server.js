81eecce9fba99a7c2cc78debf52d2f74e26f4987
sally <sallymo5354@gmail.com>
2024-03-03 23:26:36 -0500

    xzc

--- quirknotes/backend/server.js
+++ quirknotes/backend/server.js
@@ -3,16 +3,9 @@ import { MongoClient, ObjectId } from "mongodb";
 import cors from "cors";
 
 const app = express();
-const PORT = 4000;
-// const mongoURL = "mongodb://127.0.0.1:27017";
+const PORT = 4001;
+const mongoURL = "mongodb://127.0.0.1:27017";
 const dbName = "quirknotes";
-
-let mongoURL;
-if (process.env.ENV === 'Docker') {
-mongoURL = 'mongodb://mongodb:27017';
-} else {
-mongoURL = 'mongodb://127.0.0.1:27017';
-}
 
 // Connect to MongoDB
 let db;
